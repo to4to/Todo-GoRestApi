a1d3748aa9a0af58fe0b745a86e67f1d02a73765
Himanshu Kumar Singh <justjustifying@outlook.com>
2023-05-02 02:02:03 +0530

    Adding Gin+ getTodos

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "1", Item: "Read Book", Completed: false},
+	{ID: "1", Item: "Record Video", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+
+	context.IndentedJSON(http.StatusOK, todos)
+
+}
+
+func addTodo(context *gin.Context) {
+
+}
+
+func main() {
+
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:7677")
+
+}
