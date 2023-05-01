18c04d57c1e88caab5d4246f8caef4666735e08d
Himanshu Kumar Singh <justjustifying@outlook.com>
2023-05-02 02:10:47 +0530

    addTodo func added

--- main.go
+++ main.go
@@ -23,13 +23,20 @@ func getTodos(context *gin.Context) {
 }
 
 func addTodo(context *gin.Context) {
+	var newTodo todo
 
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
 func main() {
 
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
 	router.Run("localhost:7677")
 
 }
